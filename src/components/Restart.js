afdb2b8a4081da715319596face0a39c4a0f1ac3
Louis Young <louis.young0420@gmail.com>
2022-08-01 22:08:10 +0100

    restart btn v1.1

--- src/components/Restart.js
+++ src/components/Restart.js
@@ -1,3 +1,36 @@
-const Restart = () => {}
+import React from "react"
+import "../styles/modal.css"
+
+function Restart({ setRestartModal, reset, setResults }) {
+  return (
+    <div className='modal-wrapper'>
+      <div className='modal restart'>
+        <div className='takes-round restart' style={{ color: "#A8BFC9" }}>
+          Restart game?
+        </div>
+        <div className='selection-buttons'>
+          <button
+            className='quit restart'
+            onClick={() => {
+              setRestartModal(false)
+            }}
+          >
+            No, cancel
+          </button>
+          <button
+            className='next-round restart'
+            onClick={() => {
+              setRestartModal(false)
+              reset()
+              setResults([0, 0, 0])
+            }}
+          >
+            Yes, restart
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default Restart
